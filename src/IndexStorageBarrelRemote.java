5d586d111a768ef46b8a50e26b3dd68137d194fa
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-03-29 20:29:59 +0000

    minor fixes

--- src/IndexStorageBarrelRemote.java
+++ src/IndexStorageBarrelRemote.java
@@ -1,5 +1,8 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface IndexStorageBarrelRemote {
+    public ArrayList<ArrayList<String>> searchWord(String word) throws RemoteException;
+    public ArrayList<ArrayList<String>> searchWords(ArrayList<String> words) throws RemoteException;
 }
