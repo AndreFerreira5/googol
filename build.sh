17e64be47ca61d3127673d8dc96294778f41aa7f
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-04-09 20:38:42 +0100

    jar packing added to build script

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 SRC_DIR=src
 LIB_DIR=lib
 BUILD_DIR=build
+JAR_DIR=jar
 
 if [ ! -d "$BUILD_DIR" ]; then
   mkdir "$BUILD_DIR"
+fi
+
+if [ ! -d "$JAR_DIR" ]; then
+    mkdir "$JAR_DIR"
 fi
 
 find "$SRC_DIR" -name "*.java" | xargs javac -d "$BUILD_DIR" -cp "$LIB_DIR/*:$SRC_DIR"
@@ -17,3 +22,10 @@ else
     echo "Compilation failed."
     exit 1
 fi
+
+jar -cvfe "$JAR_DIR/Gateway.jar" Gateway -C "$BUILD_DIR" .
+jar -cvfe "$JAR_DIR/Client.jar" Client -C "$BUILD_DIR" .
+jar -cvfe "$JAR_DIR/IndexStorageBarrel.jar" IndexStorageBarrel -C "$BUILD_DIR" .
+jar -cvfe "$JAR_DIR/Downloader.jar" Downloader -C "$BUILD_DIR" .
+
+echo "JAR files created successfully."
